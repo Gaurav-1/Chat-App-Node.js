820c6e0212bbbfff77bca03ec5ed5ea9ccd12beb
Gaurav-1 <gauravkohli471@gmail.com>
2023-10-14 23:10:37 +0530

    V 1.0.0

---
+++ Script/chat_script.js
@@ -0,0 +1,96 @@
+if(!JSON.parse(sessionStorage.getItem('user')).userid){
+    window.location.href='/'
+}
+
+var socket = io();
+
+socket.emit('user list',JSON.parse(sessionStorage.getItem('user')).userid)
+socket.on('user list reply',disp_list)
+
+document.querySelector('#current-user').innerHTML=JSON.parse(sessionStorage.getItem('user')).username;
+function disp_list(res){
+    const user_list=document.querySelector('#user-list');
+    res.forEach(element => {
+        const div = document.createElement('div');
+        div.setAttribute('class','users')
+        div.setAttribute('id',element.userId)
+        div.innerHTML=`<h3 id='${element.userId}'>${element.username}</h3>`
+        user_list.appendChild(div)
+    });
+    document.querySelectorAll('.users').forEach(item => item.addEventListener('click',start_chat))
+}
+
+function start_chat(){
+    console.log(this);
+    const sendBy=JSON.parse(sessionStorage.getItem('user')).userid
+    const sendTo=this.id
+    const chatObj = {
+        sendBy: sendBy,
+        sendTo: sendTo
+    }
+    socket.emit('joins room',chatObj)
+    // document.querySelector('#chat_msg').innerHTML=''
+    const msg_area=document.querySelector('#msg-area')
+    msg_area.innerHTML=''
+    msg_area.innerHTML=`
+        <input type="text" id="chat_msg" name="chat_msg" placeholder="Type message here">
+        <button class="chatBtn" id=${sendTo}>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" style='fill:#175a07' viewBox="0 0 512 512">
+                <path d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"/>
+            </svg>
+        </button>`
+    document.querySelector('.chatBtn').addEventListener('click',chat)
+    socket.emit('chat data',chatObj)
+}
+
+socket.on('chat data reply',display_chat);
+
+function display_chat(resMsg){
+    let room=Object.keys(resMsg)[0]
+    let len=resMsg[room]
+    const chat_box=document.querySelector('#chat-box')
+    chat_box.innerHTML=''
+    for (let i = 1; i < len.length; i++) {
+        show_chat(len[i]);
+    }
+    chat_box.scrollTop=chat_box.scrollHeight
+}
+
+function chat(event){
+    event.preventDefault();
+    const chats=document.querySelector('#chat_msg');
+    if(!chats.value.trim()){
+        alert('Please enter a message first');
+        return;
+    }
+    const chatObj = {
+        sendBy: JSON.parse(sessionStorage.getItem('user')).userid,
+        sendTo: this.id,
+        msg: chats.value
+    }
+    console.log(chatObj);
+    socket.emit('chat-msg',chatObj)
+    chats.value=''
+}
+socket.on('reply',show_chat)
+
+function show_chat(resMsg){
+    const userid=JSON.parse(sessionStorage.getItem('user')).userid
+    const classes=(resMsg.sendBy===userid)?'user':'other'
+    const div = document.createElement('div')
+    div.setAttribute('class','chat-item')
+    div.innerHTML=`<h3 class='${classes}'>${resMsg.msg}</h3>`
+    document.getElementById('chat-box').appendChild(div)
+    const chat_box = document.querySelector('#chat-box');
+    chat_box.scrollTop=chat_box.scrollHeight
+    return;
+}
+
+document.querySelector('#logoutBtn').addEventListener('click',(e)=>{
+    fetch('/logout',{
+        method: 'DELETE'
+    }).then(()=>{
+        sessionStorage.clear();
+        window.location.href='/'
+    })
+})
---
+++ server.js
@@ -0,0 +1,219 @@
+const express = require('express');
+const app = express();
+const session= require('express-session');
+const server = require('http').createServer(app);
+const { Server } = require("socket.io");
+const path = require('path');
+const fs = require('fs');
+const {v4:uuid} = require('uuid');
+const bodyParser = require('body-parser');
+
+const io = new Server(server);
+
+const dbDir=path.join(__dirname,'/DB');
+const frontendDir=path.join(__dirname,'/FrontEnd')
+const scriptDir=path.join(__dirname,'/Script/chat_script.js')
+
+const user= {}
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.get('/',(req,res)=>{
+    if(req.session.userid){
+        res.redirect('/chat')
+        return;
+    }
+    res.sendFile(frontendDir+'/index.html');
+})
+
+app.post("/login",chk)
+
+app.delete('/logout',logout_user)
+
+app.get('/chat',show_chat);
+
+app.post('/chat',insert_chat);
+
+app.get('/Signup',(req,res)=>{
+    if(req.session.userid)
+        res.redirect('/chat')
+    else
+        res.sendFile(frontendDir+"/Signup.html")
+})
+
+app.post('/newUser',sav_user)
+
+app.get('/chat_script.js',(req,res)=>{
+    res.sendFile(scriptDir);
+})
+
+io.on('connection',(socket)=>{
+    console.log("User connnected with id: ",socket.id);
+    socket.on('disconnect',()=>{
+        console.log('User disconnected with id: ',socket.id);
+    })
+    socket.on('joins room',(data)=>{
+
+        const roomId = data.sendBy+'@'+data.sendTo;
+        const roomId2 = data.sendTo+'@'+data.sendBy
+        const file_data = JSON.parse(fs.readFileSync(dbDir+'/data.json','utf-8'))
+        const existingRoom = file_data.find(item=>Object.keys(item)[0]===roomId||Object.keys(item)[0]===roomId2);
+        let whichRoom;
+
+        file_data.find(item=>{
+            if(Object.keys(item)[0]===roomId){
+                whichRoom=roomId
+            }
+            else if(Object.keys(item)[0]===roomId2){
+                    whichRoom=roomId2
+            }
+        })
+        
+        if(!existingRoom){
+
+            file_data.push({[roomId]:[{
+                sender1: data.sendBy,
+                sender2: data.sendTo
+            }]})
+
+            fs.writeFileSync(dbDir+'/data.json',JSON.stringify(file_data,null,2))
+        }
+        user[socket.id]={
+            roomid: whichRoom
+        };
+        socket.join(whichRoom);
+
+    })
+    socket.on('chat-msg',insert_chat(socket))
+    socket.on('user list',user_list(socket))
+    socket.on('chat data',send_chat(socket))
+})
+
+server.listen(3000,(req,res)=>{
+    console.log('Hearing port 3000')
+})
+
+function show_chat(req,res){
+    if(req.session.userid)
+        res.sendFile(frontendDir+'/chat.html')
+    else
+        res.redirect('/')
+}
+
+function send_chat(socket){
+    return function (msg){
+        const room=msg.sendBy+'@'+msg.sendTo;
+        const room2 = msg.sendTo+'@'+msg.sendBy
+        
+        const chat_data=JSON.parse(fs.readFileSync(dbDir+'/data.json','utf-8'));
+        let existingRoom = chat_data.find(item=>Object.keys(item)[0]===room);
+        if(!existingRoom){
+            existingRoom=chat_data.find(item=>Object.keys(item)[0]===room2)
+        }
+        if(!existingRoom){
+            console.log('Room Not Exist');
+            return;
+        }
+        
+        const {roomid}=user[socket.id];
+        io.to(roomid).emit('chat data reply',existingRoom)
+    }
+}
+
+function user_list(socket){
+    return function (msg){
+        
+        const data=JSON.parse(fs.readFileSync(dbDir+'/users.json','utf-8'))
+        const users = data.filter(map=>map.userId!==msg)
+        io.sockets.in(socket.id).emit('user list reply',users)}
+}
+
+function insert_chat(socket){
+    return function (msg){
+        const room=msg.sendBy+'@'+msg.sendTo;
+        const room2 = msg.sendTo+'@'+msg.sendBy
+        
+        const chat_data=JSON.parse(fs.readFileSync(dbDir+'/data.json','utf-8'));
+        let existingRoom = chat_data.find(item=>Object.keys(item)[0]===room);
+        if(!existingRoom){
+            existingRoom=chat_data.find(item=>Object.keys(item)[0]===room2)
+        }
+        if(!existingRoom){
+            console.log('Room Not Exist');
+            return;
+        }
+        let whichRoom;
+        temp = chat_data.find(item=>{
+            if(Object.keys(item)[0]===room){
+                whichRoom = room;
+            }
+            else if(Object.keys(item)[0]===room2){
+                whichRoom = room2;
+            }
+        })
+        
+        existingRoom[whichRoom].push({
+            sendBy: msg.sendBy,
+            msg: msg.msg
+        })
+        const res = {
+            sendBy: msg.sendBy,
+            msg: msg.msg
+        }
+        fs.writeFileSync(dbDir+'/data.json',JSON.stringify(chat_data,null,2));
+        
+        const {roomid}=user[socket.id];
+        io.to(roomid).emit('reply',res)
+        return;
+}
+}
+
+function chk(req,res){
+    const username = req.body.username.toString().trim();
+    const password = req.body.password.toString().trim();
+
+    let data=JSON.parse(fs.readFileSync(dbDir+'/users.json','utf-8'))
+
+    data=data.find(uname=>uname.username===username);
+
+    if(data===undefined) throw new Error("User not exist")
+    let uname = data.username;
+    let psswd = data.password;
+    if(username===uname && password===psswd){
+        req.session.username=data.disUsername;
+        req.session.userid=data.userId;
+        res.json(req.session)
+    }
+    else res.status(301).json('Invalid Username and Password')
+}
+
+function sav_user(req,res){
+    console.log(req.body);
+    const data = JSON.parse(fs.readFileSync(dbDir+'/users.json','utf-8'));
+    if(data.length>0)
+        if(data.find(uname=>uname.username===req.body.username)!==undefined)
+            throw new Error('Username already exist')
+        
+        newdata={id:`${uuid()}`,disUsername: `${req.body.dis_usr}`,username:`${req.body.username}`,password:`${req.body.password}`}
+        data.push(newdata);
+        fs.writeFile(dbDir+'/users.json',JSON.stringify(data,null,2),(err)=>{
+            if(err){
+                res.josn('Signup error')
+                return;
+            }
+            res.json('/')
+            return
+        })
+}
+
+function logout_user(req,res){
+    req.session.destroy();
+    res.redirect('/')
+}
